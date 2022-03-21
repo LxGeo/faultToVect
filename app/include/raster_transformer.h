a2ff90a40e3adb1328200363597d34618aa96398
unknown <cherif@beijin.unice.fr>
2022-03-21 17:34:15 +0100

    adding multi threshhold utils && change thinning method to gauhall

--- app/include/raster_transformer.h
+++ app/include/raster_transformer.h
@@ -13,5 +13,12 @@ namespace LxGeo
 		*/
 		void thin_raster(RasterIO& raster_to_thin, RasterIO& thinned_raster, int min_value = 0);
 
+		/*
+		* Apply thinning on different binary thresholded probability map -> aggregate results
+		*/
+		// {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.85,0.9,0.92,0.95,0.99}
+		matrix multi_threshold_thinning(matrix& in_probability_matrix, std::list<double> threshold_list, std::list<double> thresh_weigths = {});
+
+
 	}
 }
--- app/src/raster_transformer.cpp
+++ app/src/raster_transformer.cpp
@@ -18,6 +18,7 @@ namespace LxGeo
 			matrix binary_image;
 			cv::threshold(raster_to_thin.raster_data, binary_image, min_value, 255, cv::THRESH_BINARY);
 			binary_image.convertTo(binary_image, CV_8U);
+                        
 
 			/*RasterIO bin_raster(raster_to_thin, binary_image);
 			boost::filesystem::path binary_output_path = boost::filesystem::path(params->temp_dir) /
@@ -25,15 +26,29 @@ namespace LxGeo
 			bin_raster.write_raster(binary_output_path.string().c_str(), true);*/
 
 			matrix thinned_binary;// = matrix::zeros(binary_image.rows, binary_image.cols, CV_8UC1);
-			cv::ximgproc::thinning(binary_image, thinned_binary, cv::ximgproc::THINNING_ZHANGSUEN);
+			cv::ximgproc::thinning(binary_image, thinned_binary, cv::ximgproc::THINNING_GUOHALL);
 			
+            int morph_size = 1;
+
+            // Create structuring element
+            matrix element = cv::getStructuringElement(
+                cv::MORPH_RECT,
+                cv::Size(2 * morph_size + 1,
+                    2 * morph_size + 1),
+                cv::Point(morph_size, morph_size));
+            cv::morphologyEx(thinned_binary, thinned_binary, cv::MORPH_CLOSE, element);
+            cv::ximgproc::thinning(thinned_binary, thinned_binary, cv::ximgproc::THINNING_GUOHALL);
+
+            /*RasterIO closed_bin_raster(raster_to_thin, binary_image);
+            boost::filesystem::path closed_binary_output_path = boost::filesystem::path(params->temp_dir) /
+                boost::filesystem::path("binary_closed.tif");
+            closed_bin_raster.write_raster(closed_binary_output_path.string().c_str(), true);*/
 
 			RasterIO bin_thin_raster(raster_to_thin, thinned_binary);
 			boost::filesystem::path binary_thin_output_path = boost::filesystem::path(params->temp_dir) /
 				boost::filesystem::path("binary_thinned.tif");
 			bin_thin_raster.write_raster(binary_thin_output_path.string().c_str(), true);
-
-			
+            			
 			raster_to_thin.raster_data.copyTo(thinned_raster.raster_data, thinned_binary);
 			//thinned_binary.convertTo(thinned_binary, raster_to_thin.raster_data.type());
 			//cv::bitwise_and(thinned_binary, raster_to_thin.raster_data, thinned_raster.raster_data);
@@ -43,6 +58,43 @@ namespace LxGeo
 			thinned_raster.write_raster(temp_thin_output_path.string().c_str(), true);*/
 
 		}
+
+		
+
+		matrix multi_threshold_thinning(matrix& in_probability_matrix, std::list<double> threshold_list, std::list<double> thresh_weigths) {
+
+			assert(threshold_list.size() > 1, "Theshhold values list must have at least 2 values!");
+
+			if (thresh_weigths.size() == 0) thresh_weigths = std::list<double>(threshold_list.size(), 1);
+
+			assert(threshold_list.size() == thresh_weigths.size(), "threshold values and weights size do not match!");
+
+			matrix aggregated(in_probability_matrix.size(), CV_32F, cv::Scalar(0));
+			double weights_sum = 0;
+
+			auto thresh_value_iter = threshold_list.begin();
+			auto thresh_weight_iter = thresh_weigths.begin();
+
+			for (; thresh_value_iter != threshold_list.end(); ++thresh_value_iter, ++thresh_weight_iter) {
+				matrix c_binary_map, thinned_binary;
+				threshold(in_probability_matrix, c_binary_map, *thresh_value_iter, 255, cv::THRESH_BINARY);
+				c_binary_map.convertTo(c_binary_map, CV_8U);
+				cv::ximgproc::thinning(c_binary_map, thinned_binary, cv::ximgproc::THINNING_ZHANGSUEN);
+
+				
+				/*RasterIO ref_raster = RasterIO(params->input_raster_path);
+				RasterIO multi_thresh_thinned_raster = RasterIO(ref_raster, thinned_binary);
+				std::string multi_thresh_thinned_path = (boost::filesystem::path(params->output_shapefile).parent_path() / "th_binary.tif").string();
+				multi_thresh_thinned_raster.write_raster(multi_thresh_thinned_path, true);
+				*/
+				cv::accumulate(thinned_binary * (*thresh_weight_iter)/255, aggregated);
+				weights_sum += *thresh_weight_iter;
+			}
+			aggregated.convertTo(aggregated, CV_8U);
+			return aggregated;
+
+		}
+
 
 	}
 }
